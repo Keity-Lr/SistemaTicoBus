02342f14a7777d1faa8a278a103dfd17765918c3
Keity-Lr <keity.lopez@ucr.ac.cr>
2026-05-17 15:52:01 -0600

    Agregar archivos de proyecto.

---
+++ SistemaTicoBus/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaTicoBus.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
